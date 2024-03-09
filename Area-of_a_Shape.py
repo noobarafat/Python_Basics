9de33a0b69ce3df1ef9191b65ebdb0d25e10bbc3
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-10 00:22:08 +0600

    solved square

--- Area-of_a_Shape.py
+++ Area-of_a_Shape.py
@@ -1,7 +1,19 @@
-# Triangle
+# # Triangle
+# base = float(input("Enter the base: "))
+# height = float(input("Enter the base: "))
 
-base = float(input("Enter the base: "))
-height = float(input("Enter the base: "))
+# area  = 0.5*base*height
+# print(area)
 
-area  = 0.5*base*height
+
+# #Square
+# area = float(input("Enter thr area:"))
+# square = area*area
+# print( square)
+
+# rectangle
+width = float(input("Enter the width:"))
+height = float(input("Enter the height:"))
+
+area = width*height
 print(area)
