e7dc785efbbb863fd5fa6cb67571c05604f72ba3
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-10 00:39:11 +0600

    practice formating

---
+++ formating.py
@@ -0,0 +1,10 @@
+
+# print(type(True))
+
+# num1 = 20
+# num2 = 30
+# print( "Sum is ",  num1+num2)
+# print(f"{num1 } + {num2} = { num1+num2}")
+
+print("K. M. Arafat Islam", end=" ")
+print("01540189333")
