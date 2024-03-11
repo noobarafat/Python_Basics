2a7686806708111e18bf342c7d68a933780aa43b
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-12 05:58:03 +0600

    set practiced

---
+++ set.py
@@ -0,0 +1,11 @@
+num1  = {1, 2, 3, 4, 5,6 ,7}
+num2 = set([4, 5, 6,7])
+# num2.add(7)
+# num2.remove(4)
+# print(num2)
+# print( 5 in num2)
+# print( 9 in num2)
+
+print(num1 | num2)
+print( num1 & num2)
+print(num1 - num2)
