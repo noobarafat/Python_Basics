1908546734132a03ab765cc1d49304b445c93196
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-03-13 11:09:07 +0600

    Import math function

---
+++ importMathFunction.py
@@ -0,0 +1,17 @@
+import math
+
+x = math.sqrt(35)
+# print(x)
+# print(int(x))
+# print(math.ceil(x))
+# print(math.floor(x))
+# print(math.pow(3, 2))
+# print(math.pi)
+# print(math.e)
+
+
+import math as m
+print(m.sqrt(47))
+
+from math import sqrt, pow
+print(pow(2, 5))
