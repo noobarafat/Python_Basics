4d74c198662e42144962a1d9859b1758a1fbb402
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-05-20 22:14:41 +0600

    Practised

---
+++ basics/conditions.py
@@ -0,0 +1,37 @@
+has_high_income = True
+has_good_credit = False
+
+# if has_high_income and has_good_credit:
+#     print("Eligible for loan")
+# else:
+#     print("Not eligible for loan")
+
+# if has_high_income or has_good_credit:
+#     print("ELigible for the loan")
+
+# is_hot = False
+# is_cold = False
+#
+# if is_hot:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+#
+# elif is_cold:
+#     print("It's a cool day")
+#     print("Wear warm clothes")
+#
+# else:
+#     print("It's a lovely day")
+#
+# print("Enjoy your day")
+
+
+# price = 1000000
+# has_good_credit = True
+#
+# if has_good_credit:
+#     down_payment = 0.1 * price
+# else:
+#     down_payment = 0.2 * price
+#
+# print(f"Down payment:${down_payment}")
