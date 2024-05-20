ebf09d5660d812fcf5c1a7d357afe49076e7364a
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-05-20 21:54:39 +0600

    Practised

---
+++ OOP/ex1.py
@@ -0,0 +1,14 @@
+class Computer:
+    def __init__(self):
+        self.name = "Navin"
+        self.age = 28
+
+    def update(self):
+        self.age = 30
+
+c1 = Computer()
+c2 = Computer()
+c1
+
+print(c1.name)
+print(c2.age)
