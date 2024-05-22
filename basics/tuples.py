2c53ade633620323a9861b49387f44e88981abdb
K. M. Arafat Islam <kmarafatislam@gmail.com>
2024-05-22 12:52:08 +0600

    Practised

--- basics/tuples.py
+++ basics/tuples.py
@@ -1,10 +1,13 @@
-students = (
-    "K. M. Arafat Islam",
-    ("Shakib Al Hasan, 21, 2.79"),
-    "Litton Das",
-)
+# students = (
+#     "K. M. Arafat Islam",
+#     ("Shakib Al Hasan, 21, 2.79"),
+#     "Litton Das",
+# )
+#
+# # students[0] = "Lamad"
+# # print(students[0])
+# print(students[1])
+# # print(students)
 
-# students[0] = "Lamad"
-# print(students[0])
-print(students[1])
-# print(students)
+numbers = (1, 2, 4, 5)
+print(numbers[3])
